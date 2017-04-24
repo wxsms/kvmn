b1a52ac0f45cb2b3acb5e1be864eab2fa5840bae
wxsm <edisond@qq.com>
2017-04-24 15:46:58 +0800

    add code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# kvmn
+# kv2 = koa 2.x + vue 2.x server side rendering
+
+> A Vue.js project with vue 2.0, vue-router and vuex starter kit for server side rendering.
+
+## Build Setup
+
+Node.js 4.x+
+
+``` bash
+npm install
+npm run build
+npm start
+```
+
+## Development Setup
+
+```bash
+npm install
+npm run dev
+```
+
+## Reference resources
+
+- [vue-ssr-starter-kit](https://github.com/doabit/vue-ssr-starter-kit)
+- [vue-ssr-demo](https://github.com/yyx990803/vue-ssr-demo)
+- [vue-hackernews-2.0](https://github.com/vuejs/vue-hackernews-2.0)
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ server.js
@@ -0,0 +1,104 @@
+'use strict'
+
+process.env.VUE_ENV = 'server'
+const isProd = process.env.NODE_ENV === 'production'
+
+const fs = require('fs')
+const path = require('path')
+const resolve = file => path.resolve(__dirname, file)
+const serialize = require('serialize-javascript')
+
+const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+
+// const app = express()
+const Koa = require('koa')
+const app = new Koa()
+const serve = require('koa-static')
+const favicon = require('koa-favicon')
+const router = require('koa-router')()
+
+// parse index.html template
+const html = (() => {
+  const template = fs.readFileSync(resolve('./index.html'), 'utf-8')
+  const i = template.indexOf('{{ APP }}')
+  // styles are injected dynamically via vue-style-loader in development
+  const style = isProd ? '<link rel="stylesheet" href="/dist/styles.css">' : ''
+  return {
+    head: template.slice(0, i).replace('{{ STYLE }}', style),
+    tail: template.slice(i + '{{ APP }}'.length)
+  }
+})()
+
+let renderer
+if (isProd) {
+  // create server renderer from real fs
+  const bundlePath = resolve('./dist/server-bundle.js')
+  renderer = createRenderer(fs.readFileSync(bundlePath, 'utf-8'))
+  console.log(renderer)
+} else {
+  require('./build/dev-server')(app, bundle => {
+    renderer = createRenderer(bundle)
+  })
+}
+
+function createRenderer (bundle) {
+  return createBundleRenderer(bundle, {
+    cache: require('lru-cache')({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    })
+  })
+}
+
+app.use(require('koa-bigpipe'))
+app.use(favicon(path.resolve(__dirname, 'src/assets/logo.png')))
+
+router.get('/dist', serve(resolve('./dist')))
+
+app.use((ctx, next) => {
+  let res = ctx.res
+  let req = ctx.req
+  if (!renderer) {
+    return res.end('waiting for compilation... refresh in a moment.')
+  }
+
+  let s = Date.now()
+  const context = {url: req.url}
+  const renderStream = renderer.renderToStream(context)
+  let firstChunk = true
+
+  ctx.write(html.head)
+  console.log(html)
+  renderStream.on('data', chunk => {
+    if (firstChunk) {
+      // embed initial store state
+      if (context.initialState) {
+        ctx.write(
+          `<script>window.__INITIAL_STATE__=${
+            serialize(context.initialState, {isJSON: true})
+            }</script>`
+        )
+      }
+      firstChunk = false
+    }
+    ctx.write(chunk)
+  })
+
+  renderStream.on('end', () => {
+    ctx.end(html.tail)
+    console.log(`whole request: ${Date.now() - s}ms`)
+  })
+
+  renderStream.on('error', err => {
+    throw err
+  })
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`)
+})
