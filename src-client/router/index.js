1d69b367f35cfd1cd138111d011bd65435a46e12
wxsm <edisond@qq.com>
2017-04-27 16:18:17 +0800

    home page not hot reload (cause mac chrome err. why?)

--- src-client/router/index.js
+++ src-client/router/index.js
@@ -10,7 +10,7 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      component: process.BROWSER ? () => System.import('../components/pages/Home.vue') : require('../components/pages/Home.vue')
+      component: require('../components/pages/Home.vue')
     },
     {
       path: '/articles',
